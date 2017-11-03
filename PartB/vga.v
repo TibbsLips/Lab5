055e4efd74c9a36bd6fbe76b3fc28b3b9097e421
TibbsLips <mallinryan@gmail.com>
2017-11-03 16:05:32 -0500

    Third

--- PartB/vga.v
+++ PartB/vga.v
@@ -1,6 +1,14 @@
-module monitor(clk); //still need to add inputs and stuff
-reg [9:0]hcount; //800 is 11 0010 0000
-reg [9:0]vcount; //525 is 10 0000 1101
+module monitor(clk, pixel,hcount,vcount,redstable,greenstable,bluestable,hsyncstable,vsyncstable);
+input clk;
+input [11:0]pixel;
+
+output reg [9:0]hcount; //800 is 11 0010 0000
+output reg [9:0]vcount; //525 is 10 0000 1101
+output reg [3:0]redstable;
+output reg [3:0]greenstable;
+output reg [3:0]bluestable;
+output reg hsyncstable;
+output reg vsyncstable;
 
 reg hsync;
 reg vsync;
@@ -30,10 +38,10 @@ always@(posedge pixclk) //This is for hcount/hsync
   begin
     hcount<=hcount+1;
     if(hcount==800)
-      begin
-        hcount<=0;
-        hsync<=hsync;
-      end
+        begin
+          hcount<=0;
+          hsync<=hsync;
+        end
     else if(hcount==659)
         begin
           hcount<=hcount;
@@ -45,12 +53,18 @@ always@(posedge pixclk) //This is for hcount/hsync
           hsync<=1;
         end
 
-    if(hcount>=640)     //no display
-      begin
-        red<=4'b0000;
-        green<=4'b0000;
-        blue<=4'b0000;
-      end
+//    if(hcount>=640)     //no display
+//      begin
+//        red<=4'b0000;
+//        green<=4'b0000;
+//        blue<=4'b0000;
+//      end
+//    else
+//      begin
+//        red<=pixel[11:8];
+//        green<=pixel[7:4];
+//        blue<=pixel[3:0];
+//      end
   end
 // is there a logic issue here? if hcount changes to 800 will it become 0 before evaluate vcount?
 always@(hcount)       //This is for vcount/vsync
@@ -74,12 +88,42 @@ always@(hcount)       //This is for vcount/vsync
               vsync<=1;
             end
 
-          if(vcount>=480) //no display
-            begin
-              red=4'b0000;
-              green=4'b0000;
-              blue=4'b0000;
-            end
+//          if(vcount>=480) //no display
+//            begin
+//              red=4'b0000;
+//              green=4'b0000;
+//              blue=4'b0000;
+//            end
+//          else
+//            begin
+//              red<=pixel[11:8];
+//              green<=pixel[7:4];
+//              blue<=pixel[3:0];
+//            end
       end
   end
+always@(hcount,vcount)
+  begin
+      if(hcount>=639|vcount>=479)
+        begin
+          red=4'b0000;
+          green=4'b0000;
+          blue=4'b0000;
+        end
+      else
+        begin
+          red<=pixel[11:8];
+          green<=pixel[7:4];
+          blue<=pixel[3:0];
+        end
+  end
+
+
+
+  end
+  DFF(red,pixclk,redstable);
+  DFF(green,pixclk,greenstable);
+  DFF(blue,pixclk,bluestable);
+  DFF4bit(hsync,pixclk,hsyncstable);
+  DFF4bit(vsync,pixclk,vsyncstable);
 endmodule
