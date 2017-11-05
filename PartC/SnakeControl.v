9b72c1904de77a333025d656a4a10b56eeef9295
TibbsLips <Mallinryan@gmail.com>
2017-11-05 15:17:31 -0600

    Eleven

--- PartC/SnakeControl.v
+++ PartC/SnakeControl.v
@@ -8,6 +8,7 @@ output reg [11:0]pixel;
 output reg [10:0]xcoord;
 output reg [10:0]ycoord;
 
+#reg [10:0]headstartposition;
 reg [6:0]length; //in case we want to expand length, make larger 40 is 0101000
 reg [4:0]width;  //10 is 01010
 
@@ -15,6 +16,8 @@ intitial
 begin
   length=7'b0101000;
   width=5'b01010;
+  xcoord=11'b00000101000;
+  ycoord=11'b00011110000;
 end
 
 always@(posedge pixclk)
