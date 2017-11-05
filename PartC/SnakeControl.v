3dc68df19c29b4c4257bb76fbe8c3c04857f2352
TibbsLips <Mallinryan@gmail.com>
2017-11-05 16:29:35 -0600

    Twelve

--- PartC/SnakeControl.v
+++ PartC/SnakeControl.v
@@ -29,19 +29,27 @@ always@(posedge pixclk)
 begin
   if((keycode1==4'b0111)&&(keycode2==4'b0101)&&(snakedirection!=2'b01))//up
     begin
-      snakedirection<=2'b11;
+      snakedirection<=2'b11;                                           //update direction
+      vertical<=length;                                                //snake is now long in the vertical direction
+      horizontal<=width;                                               //snake is fat in horizontal direction
     end
   if((keycode1==4'b0111)&&(keycode2==4'b0010)&&(snakedirection!=2'b11))//down
     begin
       snakedirection<=2'b01;
+      vertical<=length;
+      horizontal<=width;
     end
   if((keycode1==4'b0110)&&(keycode2==4'b1011)&&(snakedirection!=2'b00))//left
     begin
       snakedirection<=2b'10;
+      vertical<=width;
+      horizontal<=length;
     end
   if((keycode1==4'b0111)&&(keycode2==4'b0100)&&(snakedirection!=2'b10))//right
     begin
       snakedirection<=2'b00;
+      vertical<=width;
+      horizontal<=length;
     end
 ///above was to update position, below is to update the coordinates and the pixel
   if(snakedirection==2'b11) //up
